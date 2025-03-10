49061dd4d66a86d536bf6373409ecbb211c098a2
Alinski <alinsecareanu@protonmail.com>
2025-03-10 09:40:27 +0200

    Ci/CD updates

--- build.scala
+++ build.scala
@@ -6,9 +6,9 @@
 //> using nativeGc "none"
 
 //> using dep com.github.scopt::scopt_native0.5:4.1.0
-//> using dep com.lihaoyi::os-lib_native0.5:0.11.2
+//> using dep com.lihaoyi::os-lib_native0.5:0.11.4
 //> using dep io.circe::circe-core_native0.5:0.14.10
 //> using dep io.circe::circe-generic_native0.5:0.14.10
 //> using dep io.circe::circe-parser_native0.5:0.14.10
-//> using dep com.outr::scribe_native0.5:3.15.1
+//> using dep com.outr::scribe_native0.5:3.16.0
 //> using dep io.github.cquiroz::scala-java-time_native0.5:2.6.0
